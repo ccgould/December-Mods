10bc04e4b81bf66bce6ecf2f7fb9179dd7023de1
Senna7608 <38161982+Senna7608@users.noreply.github.com>
2019-09-21 11:31:10 +0200

    create branch and solution

---
+++ Senna/Senna/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MAC.Senna.Fahrenheit
+{
+    public static class Main
+    {
+
+
+
+
+    }
+}
