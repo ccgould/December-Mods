82ee8442cae52dcbf01c4e7723b6a0f68e914742
Matthew Henderson <oldark87@gmail.com>
2019-09-26 21:19:22 -0400

    prep for merge with passive reapers

--- WarpShield/WarpBall_Patcher.cs
+++ WarpShield/WarpBall_Patcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Harmony;
+using UnityEngine;
 
 namespace MAC.WarpShield {
     
@@ -10,7 +11,7 @@ namespace MAC.WarpShield {
     [HarmonyPatch("Warp")]
     internal class WarpBall_Warp_Patch {
         [HarmonyPrefix]
-        public static bool Prefix(WarpBall __instance)
+        public static bool Prefix(WarpBall __instance, ref GameObject target, ref Vector3 position)
         {
             return true;
         }
