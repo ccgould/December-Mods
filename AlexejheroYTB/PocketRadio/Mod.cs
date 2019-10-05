bafb3ef373bf2075ae0d9c9c1a3ae7ca82c38eee
AlexejheroYTB <alexejhero@gmail.com>
2019-10-05 17:19:19 +0300

    Update Mod.cs

--- AlexejheroYTB/PocketRadio/Mod.cs
+++ AlexejheroYTB/PocketRadio/Mod.cs
@@ -4,8 +4,10 @@ using SMLHelper.V2.Assets;
 using SMLHelper.V2.Crafting;
 using SMLHelper.V2.Handlers;
 using Story;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -29,7 +31,7 @@ namespace MAC.PocketRadio
             KnownTechHandler.SetAnalysisTechEntry(TechType.Exosuit, new TechType[] { this.TechType });
             SpriteHandler.RegisterSprite(this.TechType, SpriteManager.Get(TechType.Radio));
 
-            ItemActionHelper.RegisterAction(MouseButton.Left, this.TechType, PocketRadio.OnClick, "Play message", PocketRadio.Condition);
+            ItemActionHelper.RegisterAction(MouseButton.Left, this.TechType, PocketRadio.OnClick, "play message", PocketRadio.Condition);
         }
 
         public PocketRadioItem() : base("PocketRadio", "Pocket Radio", "Receive short-range communications and play them wirelessly.") { }
@@ -59,115 +61,123 @@ namespace MAC.PocketRadio
         }
     }
 
-    public class PocketRadio : Radio
+    public class PocketRadio : MonoBehaviour
     {
+        public bool hasMessage;
+        public FMOD_CustomEmitter playSound;
+        public FMODASRPlayer radioSound;
+
+        public static Radio.CancelIcon CancelIconEvent;
+
+        public void Awake()
+        {
+            try
+            {
+                var sound = GameObject.FindObjectsOfType<Radio>().Where(r => r.playSound != null).ToArray()[0].playSound;
+                playSound = gameObject.AddComponent<FMOD_CustomEmitter>();
+                playSound.asset = sound.asset;
+                playSound.debug = sound.debug;
+                playSound.followParent = sound.followParent;
+                playSound.playOnAwake = sound.playOnAwake;
+                playSound.restartOnPlay = sound.restartOnPlay;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("[Pocket Radio] [ERROR] Could not get playSound!");
+            }
+            try
+            {
+                var sound = GameObject.FindObjectsOfType<Radio>().Where(r => r.radioSound != null).ToArray()[0].radioSound;
+                radioSound = gameObject.AddComponent<FMODASRPlayer>();
+                radioSound.debug = sound.debug;
+                radioSound.startLoopSound = sound.startLoopSound;
+                radioSound.stopSound = sound.stopSound;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("[Pocket Radio] [ERROR] Could not get radioSound!");
+            }
+        }
+
+        public void Update()
+        {
+            ErrorMessage.AddDebug(hasMessage.ToString());
+        }
+
+        public void OnClick()
+        {
+            if (!hasMessage || IsInvoking("PlayRadioMessage")) return;
+            playSound.Play();
+            Invoke("PlayRadioMessage", 1.25f);
+
+            CancelIconEvent?.Invoke();
+        }
+
+        public void OnEnable()
+        {
+            StoryGoalManager.PendingMessageEvent -= NewRadioMessage;
+            StoryGoalManager.PendingMessageEvent += NewRadioMessage;
+            StoryGoalManager.main.PulsePendingMessages();
+        }
+
+        public void OnDisable()
+        {
+            StoryGoalManager.PendingMessageEvent -= NewRadioMessage;
+        }
+
+        public void NewRadioMessage(bool newMessages)
+        {
+            ToggleBlink(newMessages);
+            hasMessage = newMessages;
+        }
+
+        public void PlayRadioMessage()
+        {
+            StoryGoalManager.main.ExecutePendingRadioMessage();
+        }
+
+        public void ToggleBlink(bool on)
+        {
+            if (on)
+            {
+                radioSound.Play();
+            }
+            else
+            {
+                radioSound.Stop();
+            }
+        }
+
         public static bool Condition(InventoryItem item)
         {
-            return (bool)AccessTools.Field(typeof(Radio), "hasMessage").GetValue(item.item.GetComponent<PocketRadio>());
+            return item.item.GetComponent<PocketRadio>().hasMessage;
         }
 
         public static void OnClick(InventoryItem item)
         {
-            item.item.GetComponent<PocketRadio>().OnHandClick(null);
+            item.item.GetComponent<PocketRadio>().OnClick();
         }
     }
 
     public static class Patches
     {
-        [HarmonyPatch(typeof(Radio))]
-        [HarmonyPatch("IsFullHealth")]
-        public static class Radio_IsFullHealth
+        [HarmonyPatch(typeof(uGUI_RadioMessageIndicator))]
+        [HarmonyPatch("OnEnable")]
+        public static class uGUI_RadioMessageIndicator_OnEnable
         {
-            [HarmonyPrefix]
-            public static bool Prefix(Radio __instance, ref bool __result)
+            public static void Postfix(uGUI_RadioMessageIndicator __instance)
             {
-                if (__instance.GetComponent<PocketRadio>())
-                {
-                    __result = true;
-                    return false;
-                }
-                return true;
+                PocketRadio.CancelIconEvent += __instance.DisableSprite;
             }
         }
 
-        [HarmonyPatch(typeof(Radio))]
-        [HarmonyPatch("OnHandClick")]
-        public static class Radio_OnHandClick
+        [HarmonyPatch(typeof(uGUI_RadioMessageIndicator))]
+        [HarmonyPatch("OnDisable")]
+        public static class uGUI_RadioMessageIndicator_OnDisable
         {
-            [HarmonyPrefix]
-            public static bool Prefix(Radio __instance)
+            public static void Postfix(uGUI_RadioMessageIndicator __instance)
             {
-                if (__instance.GetComponent<PocketRadio>())
-                {
-                    if (!(bool)AccessTools.Field(typeof(PocketRadio), "hasMessage").GetValue(__instance) && !__instance.IsInvoking("PlayRadioMessage"))
-                    {
-                        __instance.playSound.Play();
-                        __instance.Invoke("PlayRadioMessage", 1.25f);
-                        var cancelIconEvent = AccessTools.Field(typeof(PocketRadio), "CancelIconEvent").GetValue(__instance) as Radio.CancelIcon;
-                        cancelIconEvent?.Invoke();
-                    }
-                    return false;
-                }
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Radio))]
-        [HarmonyPatch("OnHandHover")]
-        public static class Radio_OnHandHover
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(Radio __instance)
-            {
-                if (__instance.GetComponent<PocketRadio>()) return false;
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Radio))]
-        [HarmonyPatch("OnRepair")]
-        public static class Radio_OnRepair
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(Radio __instance)
-            {
-                if (__instance.GetComponent<PocketRadio>()) return false;
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Radio))]
-        [HarmonyPatch("PlayRadioRepairVO")]
-        public static class Radio_PlayRadioRepairVO
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(Radio __instance)
-            {
-                if (__instance.GetComponent<PocketRadio>()) return false;
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Radio))]
-        [HarmonyPatch("ToggleBlink")]
-        public static class Radio_ToggleBlink
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(Radio __instance, bool on)
-            {
-                if (__instance.GetComponent<PocketRadio>())
-                {
-                    //if (on)
-                    //{
-                    //    __instance.radioSound.Play();
-                    //}
-                    //else
-                    //{
-                    //    __instance.radioSound.Stop();
-                    //}
-                    return false;
-                }
-                return true;
+                PocketRadio.CancelIconEvent -= __instance.DisableSprite;
             }
         }
     }
