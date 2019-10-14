6d4cead3431e8b3cc4573261a2ac51772568b24a
AlexejheroYTB <alexejhero@gmail.com>
2019-10-14 21:56:45 +0300

    Add NoMenuPause

---
+++ AlexejheroYTB/NoMenuPause/Mod.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using UWE;
+
+namespace MAC.NoMenuPause
+{
+    public static class Mod
+    {
+        public static void Patch()
+        {
+            HarmonyInstance.Create("NoMenuPause").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(FreezeTime))]
+    [HarmonyPatch("Begin")]
+    public static class FreezeTime_Begin_Patch
+    {
+        public static bool Prefix(string userId)
+        {
+            if (userId == "IngameMenu") return false;
+            return true;
+        }
+    }
+}
