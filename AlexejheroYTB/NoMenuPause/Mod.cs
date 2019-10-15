fb1261039464bb58f1c3d677a759f54cd1ed116f
AlexejheroYTB <alexejhero@gmail.com>
2019-10-15 21:03:57 +0300

    Add options to NoMenuPause

--- AlexejheroYTB/NoMenuPause/Mod.cs
+++ AlexejheroYTB/NoMenuPause/Mod.cs
@@ -1,4 +1,8 @@
-﻿using Harmony;
+﻿using AlexejheroYTB.Common;
+using Harmony;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Options;
+using UnityEngine;
 using UWE;
 
 namespace MAC.NoMenuPause
@@ -8,6 +12,31 @@ namespace MAC.NoMenuPause
         public static void Patch()
         {
             HarmonyInstance.Create("NoMenuPause").PatchAll();
+            OptionsPanelHandler.RegisterModOptions(new Options());
+        }
+    }
+
+    public class Options : ModOptions
+    {
+        public static bool Off
+        {
+            get => PlayerPrefs.GetInt("NPM", 0).ToBool();
+            set => PlayerPrefs.SetInt("NPM", value.ToInt());
+        }
+
+        public Options() : base("No Menu Pause")
+        {
+            ToggleChanged += OnToggleChanged;
+        }
+
+        private void OnToggleChanged(object sender, ToggleChangedEventArgs e)
+        {
+            if (e.Id == "NPM") Off = e.Value;
+        }
+
+        public override void BuildModOptions()
+        {
+            AddToggleOption("NPM", "Pause while menu is open", Off);
         }
     }
 
@@ -17,7 +46,7 @@ namespace MAC.NoMenuPause
     {
         public static bool Prefix(string userId)
         {
-            if (userId == "IngameMenu") return false;
+            if (userId == "IngameMenu" && !Options.Off) return false;
             return true;
         }
     }
